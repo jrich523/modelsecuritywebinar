45a7f92a74d781a475375beffe407ed456474043
Justin Rich <justin@protectai.com>
2025-01-12 14:31:54 -0800

    local scan test

--- .github/actions/guardian-scan/scan.sh
+++ .github/actions/guardian-scan/scan.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-MODEL_FILE=$1
-BUCKET_PATH=$2
-BUCKET_MODEL_PATH="${BUCKET_PATH%/}/${MODEL_FILE#/}"
+export MODEL_FILE=$1
+export BUCKET_MODEL_PATH="s3://bucket/$MODEL_FILE"
 
 echo "Starting Guardian scan..."
 
-# check for vars/commands
-
-# aws, pip, guardian-client, jq
-
-# check and build paths
-
-if ! command -v aws; then
-    echo "AWS CLI is required!"
-    exit 1
-fi
-
-OUTPUT=$(pip install guardian-client==0.3.1 2>&1)
+OUTPUT=$(pip install guardian-client==1.1.1 2>&1)
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
     echo "❌ Failed to install guardian-client"
@@ -25,21 +13,9 @@ if [ $EXIT_CODE -ne 0 ]; then
     echo "Error output: $OUTPUT"
     exit $EXIT_CODE
 fi
-
-OUTPUT=$(aws s3 cp "${MODEL_FILE}" "${BUCKET_MODEL_PATH}" 2>&1)
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-    echo "❌ Failed to copy to S3"
-    echo "Exit code: $EXIT_CODE"
-    echo "Error output: $OUTPUT"
-    exit $EXIT_CODE
-else
-    echo "$OUTPUT"
-    echo "Successfully uploaded the model"
-fi
-
-
-
+echo "$LICENSE_ID" | docker login proxy.platform.protectai.com --username user --password-stdin
+docker compose -f .github/actions/guardian-scan/docker-compose.yaml up -d
+echo "initiating scan on $BUCKET_MODEL_PATH"
 # this will put the activity stream to stderr, and store the json object return
 OUTPUT=$(guardian-client scan --poll-interval-secs 2 "${BUCKET_MODEL_PATH}")
 EXIT_CODE=$?
@@ -52,6 +28,7 @@ fi
 echo "Result:"
 echo "$OUTPUT"
 
+docker compose -f .github/actions/guardian-scan/docker-compose.yaml down
 #format and save comment body
 
 
